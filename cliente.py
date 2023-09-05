b042a575e4076e073a797764bfbac91c192b4d7d
leonelg99 <lguerrico@alu.ing.unlp.edu.ar>
2023-09-04 23:12:25 -0300

    First lines

---
+++ cliente.py
@@ -0,0 +1,70 @@
+import pygame
+import socket
+
+# Configuración de la conexión Wi-Fi
+SERVER_IP = "192.168.1.100"  # Cambia esto por la dirección IP del MCU
+SERVER_PORT = 12345  # Puerto de escucha en el MCU
+SERVER_PASSWD = "MAW12345"  # Contraseña de la conexión Wi-Fi
+
+# Inicializar pygame
+pygame.init()
+
+# Inicializar el joystick
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)  # Selecciona el primer joystick
+joystick.init()
+
+# Inicializar la conexión al servidor
+#client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#client_socket.connect((SERVER_IP, SERVER_PORT))
+
+#client_socket.recv(1024)  # Recibe el mensaje de bienvenida del MCU
+
+#KEYS:
+#0: 1
+#1: 2
+#2: 3
+#3: 4
+#4: L1
+#5: R1
+#6: L2
+#7: R2
+#8: Select
+#9: Start
+#10: Stick Izquierdo
+#11: Stick Derecho
+### STICK DERECHO
+#### A3: Arriva: de 0.0 a -1.0
+####     Abajo: de 0.0 a 1.0
+#### A2: Izquierda: de 0.0 a -1.0
+####     Derecha: de 0.0 a 1.0
+### STICK IZQUIERDO 
+#### A1: Arriva: de 0.0 a -1.0
+####     Abajo: de 0.0 a 1.0
+#### A0: Izquierda: de 0.0 a -1.0
+####     Derecha: de 0.0 a 1.0
+
+
+
+try:
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.JOYBUTTONDOWN:
+                button = event.button
+                print(button)
+                # Envía el número del botón presionado al MCU
+                #client_socket.send(str(button).encode())
+            elif event.type == pygame.JOYAXISMOTION:
+                axis = event.axis
+                value = event.value
+                # Envía el valor del eje al MCU (puedes personalizar esta parte)
+                data = f"A{axis}:{value}"
+                print(data)
+                #client_socket.send(data.encode())
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    #client_socket.close()
+    pygame.quit()
