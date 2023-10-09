48c42a5dfe250968856dc75f849ed7e773f74828
leonelg99 <lguerrico@alu.ing.unlp.edu.ar>
2023-10-09 09:16:06 -0300

    Changes in messages

--- CLI(undertest)/conecctionv2.py
+++ CLI(undertest)/conecctionv2.py
@@ -2,18 +2,19 @@ import socket
 from shared_variables import ESP32_CAM_IP, ESP32_CAM_PORT
 
 def makeMessage(data,data2):
-    #transform data and data2 to string
-
-
     message= "cmd:"+str(data)+":"+str(data2)+"\n"  
     return message.encode()
 
 def sendMessage(data,data2):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((ESP32_CAM_IP,ESP32_CAM_PORT))
-        s.sendall(makeMessage(data,data2))
-        dat=s.recv(1024)
-        print(dat)
-        s.close()
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+         s.connect((ESP32_CAM_IP,ESP32_CAM_PORT))
+         s.sendall(makeMessage(data,data2))
+    except Exception as e:
+       add_message("Error al enviar comando: "+str(e),"error")
+    finally:
+        s.close()  
+
+
 
 
--- CLI(undertest)/connection.py
+++ CLI(undertest)/connection.py
@@ -1,80 +1,34 @@
 import socket
-import tkinter as tk
-from tkinter import messagebox
-import threading
-from shared_variables import ESP32_CAM_IP, ESP32_CAM_PORT
+from shared_variables import ESP32_CAM_IP, ESP32_CAM_PORT, SERVER_IP, SERVER_PORT
 from interfaz import WINDOW,add_message
-import time
-import sys
-connection_event = threading.Event()
- 
-def show_error_message():
-    root = tk.Tk()
-    root.withdraw()  # Oculta la ventana principal de tkinter
-    messagebox.showerror("Error de conexión", "No se pudo establecer la conexión después de 5 intentos.")
-
-def retry_connection():
-    root = tk.Tk()
-    root.withdraw()  # Oculta la ventana principal de tkinter
-    return messagebox.askyesno("Reintentar conexión", "¿Deseas intentar la conexión nuevamente?")
-  
-
-def connectionInit():   
-    global client
-    client= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    trys=0
-    while (trys<5):
-        try:
-            client.connect((ESP32_CAM_IP, ESP32_CAM_PORT))
-            message = "Connection successful\n"
-            add_message(message,"success")
-            client.sendall(message.encode())
-            dat=client.recv(1024).decode()
-            add_message("Respuesta {dat}","process")
-            connection_event.set()
-            break
-        except Exception as e:
-            trys+=1
-            print("Error al conectar: ",str(e))
-            add_message("Error al conectar: "+str(e),"error")
-            if(trys>=5):
-                show_error_message()
-            else:
-                retry = retry_connection()
-                if(retry==False):
-                   WINDOW.destroy()
-                   sys.exit()
-                else:
-                    time.sleep(0.5)    
-    incoming_message_thread = threading.Thread(target=recivirMensajes)
-    incoming_message_thread.daemon = True
-    incoming_message_thread.start()      
 
 
-def connectionClose():
-    connection_event.clear()
-    client.close()
-    add_message("Conexión cerrada","error")
-    time.sleep(1)
-    WINDOW.destroy()
-    sys.exit()
+def makeMessage(data,data2):
+    message= "cmd:"+str(data)+":"+str(data2)+"\n"  
+    return message.encode()
 
-def sendCommand(command):
+def sendMessage(data,data2):
     try:
-        client.sendall(command.encode())
-        add_message(command,"process")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+         s.connect((ESP32_CAM_IP,ESP32_CAM_PORT))
+         s.sendall(makeMessage(data,data2))
     except Exception as e:
-        add_message("Error al enviar comando: "+str(e),"error")
-        connectionClose()
+       add_message("Error al enviar comando: "+str(e),"error")
+    finally:
+        s.close()  
 
-def recivirMensajes():
-    while True:
-        try:
-            data = client.recv(1024)
-            if data:
-                add_message(data.decode(),"incoming")
-        except Exception as e:
-            add_message("Error al recibir datos: "+str(e),"error")
-            #connectionClose()
-        time.sleep(1)
-    
+
+def reciveMessages():
+   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.bind((SERVER_IP, SERVER_PORT))
+        server.listen()
+        
+        with True:
+            client_socket, client_address = server.accept()
+
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            message=data.decode()
+            add_message(message,"info")
+            client_socket.close()
